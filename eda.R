d1d33333d4311cccb90b71deb63219c1eacb159d
William <william13531@icloud.com>
2022-06-15 23:48:51 +1000

    Performed some elementary EDAs for the data set

--- eda.R
+++ eda.R
@@ -1,7 +1,101 @@
+# Exploratory Data Analysis for Milestone 1
+# The data set used provides WFI Commercial Motor Insurance claims data
+# at an individual vehicle and accident month level.
+
+# Imported libraries
 library(readr)
 library(dplyr)
 library(tidyselect)
 library(ggplot2)
 library(tidyverse)
+library("zoo")
 
-asmdata <- read.csv("ACTL31425110AssignmentData2022.csv", header=TRUE)
+data <- read.csv("ACTL31425110AssignmentData2022.csv", na.strings=c("", "NA"), header=TRUE)
+dim(data) # Consists of 1,226,044 rows and 13 columns
+
+# First recorded date of claiming
+first_claim_date = min(data$accident_month)
+
+# Last recorded date of claiming
+last_claim_date = max(data$accident_month)
+
+# List of all unique policy ids
+policy_ids = sort(unique(data$policy_id))
+
+# List of all states where vehicles are located
+risk_states = str_sort(unique(data$risk_state_name), numeric=TRUE)
+
+# List of all postcodes
+risk_postcodes = sort(unique(data$risk_postcode))
+
+# List of all years of manufacture of vehicles
+years_of_manufacture = sort(unique(data$year_of_manufacture))
+
+# List of 15 different categories of vehicle classifications
+vehicle_classes = str_sort(unique(data$vehicle_class), numeric=TRUE)
+
+# Investigate claim data per postcode
+claims_by_postcode <- data %>%
+  group_by(risk_postcode) %>%
+  summarise(TotalCarMonth = n(),
+            TotalPaid = sum(total_claims_cost, na.rm=TRUE),
+            AvgPaidPerCarMonth = TotalPaid/TotalCarMonth,
+            AvgYearMade = mean(year_of_manufacture),
+            AvgSumInsured = mean(sum_insured))
+
+claims_by_postcode = claims_by_postcode[order(-claims_by_postcode$AvgPaidPerCarMonth, -claims_by_postcode$TotalPaid), ]
+
+# As there are not enough data for every postcode for us to arrive at 
+# a reasonable conclusion, I have decided to focus on claim data per LGA
+# in Sydney and Melbourne.
+# https://postcodez.com.au/postcodes/nsw/sydney
+# Want to create a map of Sydney LGAs with colors representing claim behaviors.
+
+# Investigate claim data by quarter
+# Compare quarterly claim inflation with quarterly CPI published by ABS
+# to investigate the relationship between the two.
+
+# Added a column to include the quarter of the year for each record.
+data['quarter_in_year'] <- as.yearqtr(data$accident_month, format="%Y-%m-%d")
+
+# Consider all cars, including those without any claims.
+claims_by_quarters <- data %>%
+  group_by(quarter_in_year) %>%
+  summarise(total_car_month = n(),
+            total_paid = sum(total_claims_cost, na.rm=TRUE),
+            avg_paid_per_car = 3*total_paid/total_car_month)
+
+# Consider only claims with positive total_claims_cost
+actual_claims_by_quarters <- data %>%
+  filter(total_claims_cost > 0) %>%
+  group_by(quarter_in_year) %>%
+  summarise(total_claims = n(),
+            avg_paid_per_claim = sum(total_claims_cost)/total_claims)
+
+# Merge two tables
+claims_by_quarters <- merge(claims_by_quarters, actual_claims_by_quarters, by="quarter_in_year")
+
+# Model claim frequency with the average number of claims for a car in a quarter
+claims_by_quarters["claims_per_car"] <- c(claims_by_quarters$total_claims/claims_by_quarters$total_car_month)
+
+ggplot(data=claims_by_quarters, aes(x=quarter_in_year, y=avg_paid_per_car)) +
+  geom_bar(stat="identity", width=0.17) +
+  ggtitle("Quarterly Average Paid per Car from 2016Q3 to 2021Q2") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(y="Average paid per car", x="Quarter in Year")
+
+# Average claim size
+ggplot(data=claims_by_quarters, aes(x=quarter_in_year, y=avg_paid_per_claim)) +
+  geom_bar(stat="identity", width=0.17) +
+  ggtitle("Quarterly Average Claim Size from 2016Q3 to 2021Q2") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(y="Average Claim Size in AU$", x="Quarter in Year")
+
+# Average claim frequency
+ggplot(data=claims_by_quarters, aes(x=quarter_in_year, y=claims_per_car)) +
+  geom_bar(stat="identity", width=0.17) +
+  ggtitle("Quarterly Average Claims Frequency from 2016Q3 to 2021Q2") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(y="Average Claims Frequency", x="Quarter in Year")
+
+# Work on log changes
